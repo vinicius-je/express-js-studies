d473ed422079e38c6f2bba54abc854503a428758
vinicius-je <vinicius.je99@gmail.com>
2022-05-23 13:54:18 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const PORT = 3000;
+
+// custom middleware logger
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path}`);
+    next();
+});
+
+// built-in middleware to handle urlencode data [form data]
+app.use(express.urlencoded({ extended: false}));
+
+// built-in middleware for json
+app.use(express.json());
+
+// serve static files
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('^/$|/index(.html)?', (req, res) => {
+    // res.sendFile('./views/index.html', { root: __dirname });
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.get('/new-page(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
+});
+
+app.get('/old-page(.html)?', (req, res) => {
+    res.redirect(301, '/new-page.html'); //302 by default
+});
+
+// Route handlers
+app.get('/hello(.html)?', (req, res, next) => {
+    console.log('attempted to load hello.html');
+    next();
+}, (req, res) => {
+    res.send('Hello World')
+});
+
+const one = (req, res, next) => {
+    console.log('one');
+    next();
+}
+
+const two = (req, res, next) => {
+    console.log('two');
+    next();
+}
+
+const three = (req, res, next) => {
+    console.log('three');
+    res.send('End')
+}
+
+app.get('/chain(.html)?', [one, two, three]);
+
+app.get('/*', (req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on pont ${PORT}`);
+});
