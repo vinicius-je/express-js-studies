6eb80740d44c0bbd89986279b017bbddc230e7cb
vinicius-je <vinicius.je99@gmail.com>
2022-05-27 15:50:26 -0300

    model view controller

--- routes/api/employess.js
+++ routes/api/employess.js
@@ -1,38 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const data = {};
-data.employees = require('../../data/employees.json');
+const employeesController = require('../../controllers/employeesController');
 
 // different methods to the same route
 router.route('/')
-    .get((req, res) => {
-        res.json(data.employees);
-    })
-    .post((req, res) => {
-        const {firstname, lastname} = req.body;
-        const id = data.employees.length + 1
-        data.employees.push({id ,firstname, lastname});
-        res.send('ok');
-    })
-    .put((req, res) => {
-        const {id, firstname, lastname} = req.body;
-        data.employees.map((item) => {
-            if(item.id == id){
-                item.firstname = firstname;
-                item.lastname = lastname;
-        }});
-        res.send('ok');
-    });
+    .get(employeesController.getAllEmployees)
+    .post(employeesController.createNewEmployee)
+    .put(employeesController.updateEmployee);
 
 router.route('/:id')
-    .get((req, res) => {
-        const item = data.employees.filter((item) => item.id == req.params.id);
-        res.json(item);
-    })
-    .delete((req, res) => {
-        const item = data.employees.filter((item) => item.id != req.params.id);
-        data.employees = item;
-        res.send(data.employees);
-    });
+    .get(employeesController.getEmployee)
+    .delete(employeesController.deleteEmployee);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const cors = require('cors');
+const corsOptions = require('./config/corsOptions');
 const { logger } = require('./middleware/logEvents');
 const errorHandler = require('./middleware/errorHandler');
 
@@ -10,18 +11,6 @@ const PORT = 3000;
 // custom middleware logger
 app.use(logger);
                                                                                                 
-const whitelist = ['https://www.yoursite.com', 'http://127.0.0.1:5500', 'http://localhost:3000', 'https://www.google.com'];
-const corsOptions = {
-    origin: (origin, callback) => {
-        if (whitelist.indexOf(origin) !== -1 || !origin) {
-            callback(null, true)
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    optionsSuccessStatus: 200
-}
-
 // Cross Origin Resource Sharing
 // Open to everyone use
 // app.use(cors());
