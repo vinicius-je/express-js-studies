3320bb04329d069e98c51cf86c8f4b0f718a0b6f
vinicius-je <vinicius.je99@gmail.com>
2022-05-28 19:12:55 -0300

    user password authentication

--- server.js
+++ server.js
@@ -33,6 +33,8 @@ app.use('/subdir', express.static(path.join(__dirname, '/public')));
 app.use('/', require('./routes/root'));
 app.use('/subdir', require('./routes/subdir'));
 app.use('/employees', require('./routes/api/employess'));
+app.use('/register', require('./routes/register'));
+app.use('/auth', require('./routes/auth'));
 
 app.all('*', (req, res) => {
     res.status(404);
