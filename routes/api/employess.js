ae3d4c26e896ec74036d202c68ac90799d6eb2be
vinicius-je <vinicius.je99@gmail.com>
2022-06-04 17:19:57 -0300

    roles and permissions

--- routes/api/employess.js
+++ routes/api/employess.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const employeesController = require('../../controllers/employeesController');
-
+const ROLES_LIST = require('../../config/rolesList');
+const verifyRoles = require('../../middleware/verifyRoles');
 // different methods to the same route
 router.route('/')
     .get(employeesController.getAllEmployees)
-    .post(employeesController.createNewEmployee)
-    .put(employeesController.updateEmployee);
+    .post(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), employeesController.createNewEmployee)
+    .put(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), employeesController.updateEmployee);
 
 router.route('/:id')
     .get(employeesController.getEmployee)
-    .delete(employeesController.deleteEmployee);
+    .delete(verifyRoles(ROLES_LIST.Admin), employeesController.deleteEmployee);
 
 module.exports = router;
