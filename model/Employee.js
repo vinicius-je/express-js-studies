249f58fe2d289abdb9c0fc47e3950bbec7673bcf
vinicius-je <vinicius.je99@gmail.com>
2022-06-13 17:57:29 -0300

    mongoDB Schemas and Data Models

---
+++ model/Employee.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const employeeSchema = new Schema({
+    firstname: {type: String, required: true},
+    lastname: {type: String, required: true}
+});
+
+// data model
+module.exports = mongoose.model('Employee', employeeSchema);
